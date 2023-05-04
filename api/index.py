b53a92929fa5a492b45c2d95510b323775812f68
IssaC114 <yongxi696@gmail.com>
2023-05-04 16:21:06 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,6 +17,15 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('tuJK6H6vmCfza8sfGG8ARXxxkeFxLKuBJxua+Ucg9IbJi+hM/0aw0KyNWgepqZVVSHmMLQmJXn0jxIJW0O1deku1tuQUMPx7BHJgQ7l4Ci1rRATFSD+5pGfmMYf35BVcNbYbCPEwiScJQ4HJ/vqGAgdB04t89/1O/w1cDnyilFU=')
 webhook_handler = WebhookHandler('72fb67decf0b6155beac93cfe8bd50bb')
+
+openai.api_key = os.environ("OPENAI_API_KEY")
+input_text = "今天天氣很好，請用中文回答。請做一首跟天氣有關的詩"
+
+# 設定 GPT-3.5 模型的檢索引擎
+model_engine = "text-davinci-003"
+
+# 設定生成的文本長度
+output_length = 50
 
 @app.route("/")
 def home():
@@ -43,10 +53,10 @@ def callback():
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     response = openai.Completion.create(
-    engine=model_engine,
-    prompt=input_text,
-    max_tokens=output_length,
-)
+        engine=model_engine,
+        prompt=input_text,
+        max_tokens=output_length,
+    )
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
